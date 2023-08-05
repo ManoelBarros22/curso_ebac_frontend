0251c2d64e1340fe32f161a9a2f9d44cf8fff068
ManoelBarros22 <126240510+ManoelBarros22@users.noreply.github.com>
2023-08-05 14:51:52 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const multiplicar = (a, b) => { return 'Total da multiplicação: ' + a * b; };
+
+console.log(multiplicar(7, 3));
+
+
+const ola = (nome) => { return "Olá " + nome; };
+
+console.log(ola('Manoel'));
---
+++ src/script.ts
@@ -0,0 +1,10 @@
+
+
+const multiplicar = (a: number,b: number):number | string => {return 'Total da multiplicação: ' + a * b};
+
+console.log(multiplicar(7,3)); 
+
+
+const ola = (nome:string):string => {return "Olá " + nome}
+
+console.log(ola('Manoel')); 
